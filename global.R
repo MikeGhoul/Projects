18ee1faa468eb7929398f5290af417b9df2ad642
MikeGhoul <mike.ghoul5@gmail.com>
2017-07-14 21:43:46 -0400

    Add files via upload

--- global.R
+++ global.R
@@ -4,13 +4,13 @@ regions_summ_mean
 
 
 regions_summ_mean$year <- as.numeric(regions_summ_mean$year)
+
+
 country_summ_mean
 
 # create variable with colnames as choice
 choice <- colnames(country_summ_mean)[c(-1, -2, -3, -4)]
-
-
-Region <- regions_summ_mean$Region
+choice2 <- colnames(regions_summ_mean)[c(-1, -2)]
 
 
 library(DT)
@@ -20,4 +20,6 @@ library(DT)
 library(shiny)
 library(googleVis)
 library(plotly)
+
+
 
